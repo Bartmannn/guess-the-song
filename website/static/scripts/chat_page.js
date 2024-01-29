daf749819e0597e698781272932bb13eed26a4b7
kf0r <krzysfor@gmail.com>
2024-01-29 23:27:23 +0100

    JS documentation

--- website/static/scripts/chat_page.js
+++ website/static/scripts/chat_page.js
@@ -1,13 +1,31 @@
+/**
+ * Execute the following code after the DOM has been fully loaded.
+ * @event DOMContentLoaded
+ */
 document.addEventListener("DOMContentLoaded", () => {
-    // Make "enter" key submit message
+    /**
+     * Reference to the 'user_message' input field HTML element.
+     * @type {HTMLInputElement}
+     */
     let msg = document.querySelector("#user_message");
+
+    /**
+     * Add an event listener to the 'keyup' event for the 'user_message' input field.
+     * @event keyup
+     * @param {KeyboardEvent} event - The keyup event object.
+     */
+
     msg.addEventListener("keyup", event => {
+        /**
+         * Key code for the "Enter" key.
+         * @const
+         * @type {number}
+         */
+        const enterKeyCode = 13;
         event.preventDefault();
-        if (event.keyCode === 13) {
+        if (event.keyCode === enterKeyCode) {
             document.querySelector("#send_message").click();
         }
     })
-
-    
 
 });
--- website/static/scripts/index.js
+++ website/static/scripts/index.js
@@ -1,9 +1,33 @@
+/**
+ * Reference to the 'Copy to Clipboard' button HTML element.
+ * @type {HTMLElement}
+ */
 var copyButton = document.getElementById("copyToClipboard");
+
+/**
+ * Reference to the 'Next Round' button HTML element.
+ * @type {HTMLElement}
+ */
 var nextRoundButton = document.getElementById("next_round_button");
+
+/**
+ * Reference to the 'Start Game' button HTML element.
+ * @type {HTMLElement}
+ */
 var startGameButton = document.getElementById("start_game_button");
 
+
+/**
+ * Variable indicating the administrative status.
+ * @type {boolean}
+ */
 console.log(is_admin);
 
+/**
+ * Add an onclick event listener to the 'Copy to Clipboard' button.
+ * Copies the content of the 'invite_link' field to the clipboard.
+ * @event click
+ */
 copyButton.onclick = () => {
     var copyText = document.getElementById("invite_link");
     copyText.select();
@@ -11,6 +35,11 @@ copyButton.onclick = () => {
     navigator.clipboard.writeText(copyText.value);
 }
 
+
+/**
+ * Disable and apply grayscale filters to buttons if the user is not an admin.
+ * @type {boolean}
+ */
 if (is_admin == "False") {
     startGameButton.style.filter = "grayscale(50%)";
     nextRoundButton.style.filter = "grayscale(75%)";
